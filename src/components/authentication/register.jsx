f897cc3d037411cab96e1ea7fab2acdff1177813
Bahram Afsharipoor <46548396+develop1992@users.noreply.github.com>
2020-08-15 16:06:05 -0500

    added form validation to authentication page (#1)

--- src/components/authentication/register.jsx
+++ src/components/authentication/register.jsx
@@ -1,39 +1,95 @@
 import React from "react";
-import loginImg from "../../images/authentication.svg";
+import authImg from "../../images/authentication.svg";
+import { Form, Button } from 'react-bootstrap';
 
 export class Register extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            validated: false,
+            isBtnClicked: false
+        }
+    }
+
+    register = (event) => {
+
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+
+            // stop form from being submitted to server
+            event.preventDefault();
+            event.stopPropagation();
+
+            // form is invalid so card
+            this.props.updateSideCard(false);
+
+        }
+        else {
+            // form is valid so card
+            this.props.updateSideCard(true);
+        }
+
+        this.setState({ validated: true, isBtnClicked: true });
+    };
+
+    onInputChange = (event) => {
+
+        if(this.state.isBtnClicked) {
+            
+            const form = event.currentTarget.parentNode.parentNode;
+            if (form.checkValidity()) {
+                // form is valid so card
+                this.props.updateSideCard(true);
+            }
+            else {
+                // form is invalid so card
+                this.props.updateSideCard(false);
+            }
+
+        }
+
     }
 
     render() {
 
         return (
             <div className="base-container" ref={this.props.containerRef}>
+
                 <div className="header">Register</div>
+
                 <div className="content">
+
                     <div className="image">
-                        <img src={loginImg} />
+                        <img src={authImg} alt="authentication" />
                     </div>
-                    <div className="form">
-                        <div className="form-group">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" name="username" placeholder="john.doe@gmail.com" />
-                            <small id="emailHelp" className="text-muted" style={{marginTop: -20}}>Please enter your email address</small>
+
+                    <Form className="form" noValidate validated={this.state.validated} onSubmit={this.register}>
+
+                        <Form.Group>
+                            <Form.Label htmlFor="username">Username</Form.Label>
+                            <Form.Control onChange={this.onInputChange} autoComplete="off" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" type="email" name="username"
+                                placeholder="john.doe@gmail.com" required></Form.Control>
+                            <Form.Text className="text-muted" style={{ marginTop: -20 }}>Please enter your email address</Form.Text>
+                            <Form.Control.Feedback style={{ textAlign: 'left' }} type="invalid">Please type a valid email address</Form.Control.Feedback>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label htmlFor="password">Password</Form.Label>
+                            <Form.Control minLength="6" maxLength="40" onChange={this.onInputChange} autoComplete="off" type="password" name="password" placeholder="******" required></Form.Control>
+                            <Form.Text className="text-muted" style={{ marginTop: -20 }}>Must be 6-40 characters</Form.Text>
+                            <Form.Control.Feedback style={{ textAlign: 'left' }} type="invalid">Password must be between 6-40 characters</Form.Control.Feedback>
+                        </Form.Group>
+
+                        <div className="footer">
+                            <Button type="submit" className="btn">Register</Button>
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" placeholder="******" />
-                            <small id="passwordHelp" className="text-muted" style={{marginTop: -20}}>Must be 6-40 characters</small>
-                        </div>
-                    </div>
+
+                    </Form>
+
                 </div>
-                <div className="footer">
-                    <button type="button" className="btn">
-                        Register
-                    </button>
-                </div>
+
             </div>
         );
 
